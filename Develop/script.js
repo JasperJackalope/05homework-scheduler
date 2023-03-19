c039aec4bbc38910875bc43ca18caa5267e93afd
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-19 14:23:29 -0700

    Addded Date to Script

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
+var todayDate = dayjs();
+$('#currentDay').text(todayDate.format('MMM D, YYYY'));
+
+console.log(todayDate);
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
