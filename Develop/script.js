1bfdaa08d2bdbb2a5672f8ce43669fe5c8c2ad67
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-20 22:19:25 -0700

    Debugged and Condensed

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,12 @@
 var todayDate = dayjs();
 var hour = todayDate.hour();
 
-$('#currentDay').text(todayDate.format('MMM D, YYYY'));
+$('#currentDay').text(todayDate.format('ddd, MMM D, YYYY'));
 
 console.log(todayDate.hour());
 
 $(function () {
+  console.log("Friday night and the lights are low...")
   for (var i = 9; i <= 17; i++) {
     var row = $('#hour-' + i);
     if (hour < i && hour >= 0) {
@@ -19,7 +20,7 @@ $(function () {
 });
 
 $('button').on('click', function() {
-  console.log("Dancy Party!");
+  console.log("Dance Party!");
   $('textarea[id^="TA"]').filter(function() {
     var idNum = parseInt($(this).attr('id').substring(2));
     return idNum >= 9 && idNum <= 17;
@@ -27,20 +28,19 @@ $('button').on('click', function() {
     var textValue = $(this).val();
     var key = 'To Do List ' + $(this).attr('id');
     localStorage.setItem(key, textValue);
-    console.log('Saved:', textValue, 'with key:', key);
+    console.log('Saved:' + textValue);
   });
 });
 
 $(document).ready(function() {
+  console.log('Smokin!')
   $('textarea[id^="TA"]').filter(function() {
     var idNum = parseInt($(this).attr('id').substring(2));
     return idNum >= 9 && idNum <= 17;
   }).each(function() {
     var key = 'To Do List ' + $(this).attr('id');
-    var textValue = localStorage.getItem(key);
-    if (textValue !== null) {
-      $(this).val(textValue);
-      console.log('Loaded:', textValue, 'with key:', key);
-    }
+    var textValueGet = localStorage.getItem(key);
+    if (textValueGet !== null) {
+      $(this).val(textValueGet)}
   });
 });
