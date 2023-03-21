76e1d6569667bb01574717606eb5316c1431c74c
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-20 21:59:21 -0700

    Looped Code for Element Storage

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,8 @@ $('#currentDay').text(todayDate.format('MMM D, YYYY'));
 console.log(todayDate.hour());
 
 $(function () {
-  var hour = todayDate.hour();
-
   for (var i = 9; i <= 17; i++) {
     var row = $('#hour-' + i);
-
     if (hour < i && hour >= 0) {
       row.addClass("future");
     } else if (hour >= i + 1 && hour < 24) {
@@ -22,24 +19,28 @@ $(function () {
 });
 
 $('button').on('click', function() {
-  const textValue = $('textarea').val();
-  localStorage.setItem('To Do List', textValue);
-  console.log('Saved:', textValue);
+  console.log("Dancy Party!");
+  $('textarea[id^="TA"]').filter(function() {
+    var idNum = parseInt($(this).attr('id').substring(2));
+    return idNum >= 9 && idNum <= 17;
+  }).each(function() {
+    var textValue = $(this).val();
+    var key = 'To Do List ' + $(this).attr('id');
+    localStorage.setItem(key, textValue);
+    console.log('Saved:', textValue, 'with key:', key);
+  });
 });
 
-
-// const savedText = localStorage.getItem('event');
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+$(document).ready(function() {
+  $('textarea[id^="TA"]').filter(function() {
+    var idNum = parseInt($(this).attr('id').substring(2));
+    return idNum >= 9 && idNum <= 17;
+  }).each(function() {
+    var key = 'To Do List ' + $(this).attr('id');
+    var textValue = localStorage.getItem(key);
+    if (textValue !== null) {
+      $(this).val(textValue);
+      console.log('Loaded:', textValue, 'with key:', key);
+    }
+  });
+});
