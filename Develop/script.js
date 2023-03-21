55ba368a86ad8ab616f6e3331a9a466a602a23cc
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-20 17:05:38 -0700

    Color Change CSS Function Added

--- Develop/script.js
+++ Develop/script.js
@@ -2,21 +2,108 @@ var todayDate = dayjs();
 
 $('#currentDay').text(todayDate.format('MMM D, YYYY'));
 
-$('.row').each(function() {
-    var time = $(this).data("time");
-    if (time < Date.now()) {
-      $(this).addClass("past");
-    } else if (time > Date.now()) {
-      $(this).addClass("future");
-    } else {
-      $(this).addClass("present");
-    }
-});
+console.log(todayDate.hour());
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
+  var hour = todayDate.hour();
+  var h9Row = $('#hour-9');
+  if (hour < 9 && hour >= 0) {
+    h9Row.addClass("future");
+  } else if (hour >= 10 && hour < 24) {
+    h9Row.addClass("past");
+  } else {
+    h9Row.addClass("present");
+}});
+
+$(function () {
+  var hour = todayDate.hour();
+  var h10Row = $('#hour-10');
+  if (hour < 10 && hour >= 0) {
+    h10Row.addClass("future");
+  } else if (hour >= 11 && hour < 24) {
+    h10Row.addClass("past");
+  } else {
+    h10Row.addClass("present");
+}});
+
+$(function () {
+  var hour = todayDate.hour();
+  var h11Row = $('#hour-11');
+  if (hour < 11 && hour >= 0) {
+    h11Row.addClass("future");
+  } else if (hour >= 12 && hour < 24) {
+    h11Row.addClass("past");
+  } else {
+    h11Row.addClass("present");
+}});
+
+$(function () {
+  var hour = todayDate.hour();
+  var h12Row = $('#hour-12');
+  if (hour < 12 && hour >= 0) {
+    h12Row.addClass("future");
+  } else if (hour >= 13 && hour < 24) {
+    h12Row.addClass("past");
+  } else {
+    h12Row.addClass("present");
+}});
+
+$(function () {
+  var hour = todayDate.hour();
+  var h13Row = $('#hour-13');
+  if (hour < 13 && hour >= 0) {
+    h13Row.addClass("future");
+  } else if (hour >= 14 && hour < 24) {
+    h13Row.addClass("past");
+  } else {
+    h13Row.addClass("present");
+}});
+
+$(function () {
+    var hour = todayDate.hour();
+    var h14Row = $('#hour-14');
+    if (hour < 14 && hour >= 0) {
+      h14Row.addClass("future");
+    } else if (hour >= 15 && hour < 24) {
+      h14Row.addClass("past");
+    } else {
+      h14Row.addClass("present");
+}});
+
+$(function () {
+    var hour = todayDate.hour();
+    var h15Row = $('#hour-15');
+    if (hour < 15 && hour >= 0) {
+      h15Row.addClass("future");
+    } else if (hour >= 16 && hour < 24) {
+      h15Row.addClass("past");
+    } else {
+      h15Row.addClass("present");
+  }});
+
+$(function () {
+    var hour = todayDate.hour();
+    var h16Row = $('#hour-16');
+    if (hour < 16 && hour >= 0) {
+      h16Row.addClass("future");
+    } else if (hour >= 17 && hour < 24) {
+      h16Row.addClass("past");
+    } else {
+      h16Row.addClass("present");
+}});
+
+$(function () {
+    var hour = todayDate.hour();
+    var h17Row = $('#hour-17');
+    if (hour < 17 && hour >= 0) {
+      h17Row.addClass("future");
+    } else if (hour >= 18 && hour < 24) {
+      h17Row.addClass("past");
+    } else {
+      h17Row.addClass("present");
+  }});
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -35,4 +122,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
